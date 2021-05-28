86d5df0152db27431378498327e46022eca6732f
Yonohi <75024795+Yonohi@users.noreply.github.com>
2021-05-28 09:23:56 +0200

    Modification et anglais

--- scrap_cat.py
+++ scrap_cat.py
@@ -1,16 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-from scrap_livre import scraper
+from scrap_book import scraper
 import os
 
 
 def scraper_cat(url_cat, categorie="erreur"):
-    # On cree les dossiers
+    """Function calling the scraper from scrap_book and applying it for a category.
+    
+    :param url_cat: Url of the wanted category
+    :param categorie: Name of the category
+    """
+    # Making of directory
     os.makedirs("Info_recolte", exist_ok=True)
     os.makedirs(f"Info_recolte/{categorie}", exist_ok=True)
     os.makedirs(f"Info_recolte/{categorie}/Images", exist_ok=True)
 
-    # Ecriture dans le fichier CSV
+    # Writing in the CSV file
     with open(f"Info_recolte/{categorie}/{categorie}.csv", "w") as file:
         file.write("product_page_url,\
             universal_product_code,\
@@ -23,18 +28,18 @@ def scraper_cat(url_cat, categorie="erreur"):
                    review_rating,\
                     image_url" + "\n")
 
-        # Mettre les url des pages d'une categorie dans une liste
+        # Put the pages's url of a category in a list
         list_url_page = [url_cat]
         for url_page in list_url_page:
             response = requests.get(url_page)
             soup = BeautifulSoup(response.text, features="html.parser")
-            # Selection des livres d'une page
+            # Selection of books in a page
             list_info = soup.find("ol", {"class": "row"}).findAll("h3")
             for livre in list_info:
                 url_livre = "http://books.toscrape.com/catalogue" + livre.find("a")["href"].replace("../../..", "")
-                # Ecriture dans le fichier csv des donnees recoltees
+                # Writing the collected data in the csv file
                 file.write(scraper(url_livre, categorie))
-            # On teste si une page suivante existe
+            # Test if a following page exists
             try:
                 next_page = soup.find("li", {"class": "next"}).find("a")
                 url_test = url_cat.replace("index.html", next_page["href"])
